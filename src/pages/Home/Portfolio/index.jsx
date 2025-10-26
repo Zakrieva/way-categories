6084189c971f85a56ceae247d9ee2ce2034bf032
Zakrieva <zakrieva34t@mail.ru>
2025-10-26 19:13:47 +0300

    fix:

--- src/pages/Home/Portfolio/index.jsx
+++ src/pages/Home/Portfolio/index.jsx
@@ -3,8 +3,8 @@ import Button from "../../../components/Button";
 import img1 from "../../../assets/img/home/img4.jpg";
 import img2 from "../../../assets/img/home/img5.jpg";
 import styles from "./index.module.sass";
-import vectorLeft from "../../../assets/svg/VectorLeft.svg";
-import vectorRight from "../../../assets/svg/VectorRight.svg";
+// import vectorLeft from "../../../assets/svg/VectorLeft.svg";
+// import vectorRight from "../../../assets/svg/VectorRight.svg";
 const Portfolio = () => {
   return (
     <div className="wrapper">
@@ -12,12 +12,12 @@ const Portfolio = () => {
         <div className={styles.title}>
           <h1>Наше портфолио</h1>
           <div className={styles.buts}>
-            <div className={styles.btn}>
+            {/* <div className={styles.btn}>
               <Button title={<img src={vectorLeft} alt="" />} />
             </div>
             <div className={styles.btn}>
               <Button title={<img src={vectorRight} alt="" />} />
-            </div>
+            </div> */}
           </div>
         </div>
 
